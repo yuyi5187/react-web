7c39633e8f04631bb47423e2b036c72b8e1da38a
yuyi5187 <kimyy200063@gmail.com>
2021-12-19 00:21:42 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
 
   return (<Router>
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/movie/:id" element={<Detail/>}/>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail/>}/>
     </Routes>
   </Router>
   );
